7089e946131734adbfbc230f456ba354d5b698e7
Ahmad Abdullah <ahmadnofly@gmail.com>
2023-06-06 18:58:36 +0300

    add mod test

--- README.md
+++ README.md
@@ -12,3 +12,5 @@
 - Write example requests for the server
 - Exceptions for inserting users ,better error messages
 
+
+INSERT INTO roles(name) VALUES('ROLE_USER');INSERT INTO roles(name) VALUES('ROLE_MODERATOR');INSERT INTO roles(name) VALUES('ROLE_ADMIN');
--- src/main/java/com/dev/airmap/controllers/TestController.java
+++ src/main/java/com/dev/airmap/controllers/TestController.java
@@ -19,8 +19,14 @@ public class TestController {
 
     @GetMapping("/user")
     @PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
+    public String userAccess() {
+      return "User Content.";
+    }
+  
+    @GetMapping("/mod")
+    @PreAuthorize("hasRole('MODERATOR')")
     public String moderatorAccess() {
-        return "Moderator Board.";
+      return "Moderator Board.";
     }
 
     @GetMapping("/admin")
